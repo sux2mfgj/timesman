dc5309221dd93b71f33769020886061c5ec61341
Shunsuke Mie <sux2mfgj@gmail.com>
2024-10-19 22:29:50 +0900

    app: complete a functionality of first step

--- timesman-app/src/main.rs
+++ timesman-app/src/main.rs
@@ -1,13 +1,65 @@
 use eframe::egui::ScrollArea;
+use reqwest;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+struct RequestData {
+    comment: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Comments {
+    id: i64,
+    comment: String,
+    created_at: chrono::NaiveDateTime,
+}
 
 struct TimesManApp {
     input_text: String,
+    server: String,
+    list: Vec<Comments>,
+}
+
+impl TimesManApp {
+    fn post(&mut self, text: &String) {
+        let client = reqwest::blocking::Client::new();
+        let data = RequestData {
+            comment: text.clone(),
+        };
+
+        let url = self.server.clone() + "/append";
+
+        let res = client
+            //.post("http://localhost:8080/append")
+            .post(url)
+            .json(&data)
+            .send();
+
+        let com = Comments {
+            id: 0,
+            comment: text.clone(),
+            created_at: chrono::Local::now().naive_local(),
+        };
+
+        self.list.push(com);
+    }
+
+    fn get_list(server: &String) -> Vec<Comments> {
+        let url = server.clone() + "/list";
+        reqwest::blocking::get(url)
+            .unwrap()
+            .json::<Vec<Comments>>()
+            .unwrap()
+    }
 }
 
 impl Default for TimesManApp {
     fn default() -> Self {
+        let server = "http://localhost:8080";
         Self {
             input_text: "".to_owned(),
+            server: server.to_string(),
+            list: TimesManApp::get_list(&server.to_string()),
         }
     }
 }
@@ -20,10 +72,10 @@ impl eframe::App for TimesManApp {
             let scroll_area = ScrollArea::vertical().max_height(200.0).auto_shrink(false);
             scroll_area.show(ui, |ui| {
                 ui.vertical(|ui| {
-                    for i in 1..30 {
+                    for comment in &self.list {
                         ui.horizontal(|ui| {
-                            ui.label("20:21");
-                            ui.label(format!("test {i}"));
+                            ui.label(format!("{}", comment.created_at));
+                            ui.label(&comment.comment);
                         });
                     }
                 });
@@ -39,9 +91,9 @@ impl eframe::App for TimesManApp {
                     return;
                 }
 
-                let _text = self.input_text.clone();
+                let text = self.input_text.clone();
 
-                //TODO: send to server
+                self.post(&text);
                 self.input_text.clear();
             }
         });
@@ -53,9 +105,31 @@ fn main() -> eframe::Result {
         viewport: egui::ViewportBuilder::default().with_inner_size([400.0, 320.0]),
         ..Default::default()
     };
+
     eframe::run_native(
         "My egui App",
         options,
         Box::new(|_| Ok(Box::<TimesManApp>::default())),
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn json_test() {
+        let data = RequestData {
+            comment: "hello".to_string(),
+        };
+        let j = serde_json::to_string(&data).unwrap();
+        println!("{}", j);
+        assert_eq!(j, r#"{"comment":"hello"}"#);
+    }
+
+    #[test]
+    fn get_list() {
+        let vec = TimesManApp::get_list(&"http://localhost:8080/list".to_string());
+        println!("{:?}", vec);
+    }
 }
