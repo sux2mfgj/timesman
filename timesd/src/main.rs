3657b14511798fcef80db254529f4aeed2bccdb4
Shunsuke Mie <mie@MiMac.local>
2024-10-23 14:40:06 +0900

    implement post api

--- timesd/src/main.rs
+++ timesd/src/main.rs
@@ -69,6 +69,40 @@ async fn create_times(ctx: web::Data<AppContext>, req: web::Json<CreateTimesRequ
     HttpResponse::Ok().body(serde_json::to_string(&resp).unwrap())
 }
 
+#[derive(Serialize)]
+struct Post {
+    id: i64,
+    times_id: i64,
+    post: String,
+    created_at: chrono::NaiveDateTime,
+    updated_at: Option<chrono::NaiveDateTime>,
+}
+
+#[derive(Serialize)]
+struct GetPostResponse {
+    base: ResponseBase,
+    posts: Vec<Post>
+}
+
+async fn get_posts(ctx: web::Data<AppContext>, path: web::Path<i64>)
+    -> impl Responder {
+
+    let tid = path.into_inner();
+    let posts = sqlx::query_as!(Post, r#"select * from posts where times_id = $1"#,
+                                tid)
+        .fetch_all(&ctx.db).await.unwrap();
+
+    let resp = GetPostResponse {
+        base: ResponseBase{ 
+            status: 0,
+            text: "Ok".to_string()
+        },
+        posts: posts,
+    };
+
+    HttpResponse::Ok().body(serde_json::to_string(&resp).unwrap())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let pool = SqlitePool::connect("./database.db").await.unwrap();
@@ -80,7 +114,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(ctx.clone()))
             .route("/times", web::get().to(get_times))
             .route("/times", web::post().to(create_times))
-            //.route("/times/{tid}", web::get().to(get_posts))
+            .route("/times/{tid}", web::get().to(get_posts))
             //.route("/times/{tid}", web::post().to(post_post))
     })
     .bind("localhost:8080")?
