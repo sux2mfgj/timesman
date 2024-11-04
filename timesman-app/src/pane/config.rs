51d0507c98a6f7c95c7ab303ee3be3da6d2f8419
Shunsuke Mie <sux2mfgj@gmail.com>
2024-11-04 21:14:32 +0900

    add config

---
+++ timesman-app/src/pane/config.rs
@@ -0,0 +1,34 @@
+use crate::app::{Event, Pane};
+
+use super::pane_menu;
+
+pub struct ConfigPane {}
+
+impl Pane for ConfigPane {
+    fn update(
+        &mut self,
+        ctx: &egui::Context,
+        _frame: &mut eframe::Frame,
+        _req: &crate::req::Requester,
+    ) -> Event {
+        let mut event = Event::Nothing;
+        egui::TopBottomPanel::top("top").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("Times", |ui| {
+                    if let Some(e) = pane_menu(ui) {
+                        event = e;
+                        ui.close_menu();
+                    }
+                });
+            });
+        });
+
+        event
+    }
+}
+
+impl ConfigPane {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
