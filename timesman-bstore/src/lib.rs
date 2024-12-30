b49bb1f7dd677f63e591023e389bb8933a505f28
Shunsuke Mie <sux2mfgj@gmail.com>
2024-12-30 14:31:10 +0900

    bstore: implement the format for Times

--- timesman-bstore/src/lib.rs
+++ timesman-bstore/src/lib.rs
@@ -31,6 +31,20 @@ pub struct Times {
     pub updated_at: Option<chrono::NaiveDateTime>,
 }
 
+impl std::fmt::Display for Times {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(utime) = self.updated_at {
+            write!(
+                f,
+                "{} {} {} {}",
+                self.id, self.title, self.created_at, utime
+            )
+        } else {
+            write!(f, "{} {} {}", self.id, self.title, self.created_at)
+        }
+    }
+}
+
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Post {
     pub id: i64,
