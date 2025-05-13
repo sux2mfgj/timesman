0c32c515b5c829739815eaf14f569382a035a9a9
Shunsuke Mie <sux2mfgj@gmail.com>
2025-05-13 20:26:43 +0900

    bstore: fix json store

--- timesman-bstore/src/json.rs
+++ timesman-bstore/src/json.rs
@@ -1,19 +1,37 @@
-use serde::{Deserialize, Serialize};
-use std::fs;
-use std::io::{BufWriter, Write};
-use std::{fs::File, path::PathBuf};
-
-use super::{Post, Store, Times};
+use super::{PostStore, Store, TimesStore, TodoStore};
 use async_trait::async_trait;
+use timesman_type::{File, Pid, Post, Tdid, Tid, Times, Todo};
+
+use std::{fs, path::PathBuf};
+
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 pub struct JsonStore {
-    data: Data,
+    path: PathBuf,
+    file: fs::File,
 }
 
-#[derive(Serialize, Deserialize)]
-struct Data {
-    times: Times,
-    posts: Vec<Post>,
+impl JsonStore {
+    pub fn new(path: PathBuf, is_create: bool) -> Result<Self, String> {
+        let file = if is_create {
+            if path.exists() {
+                return Err(format!("The file {:?} already exists", path));
+            }
+
+            fs::File::open(&path).map_err(|e| format!("{e}"))?
+        } else {
+            if !path.exists() {
+                return Err(format!("The file {:?} is not found", path));
+            }
+
+            fs::File::create(&path).map_err(|e| format!("{e}"))?
+        };
+
+        let store = Self { path, file };
+
+        Ok(store)
+    }
 }
 
 #[async_trait]
@@ -22,112 +40,20 @@ impl Store for JsonStore {
         Ok(())
     }
 
-    async fn get_times(&mut self) -> Result<Vec<super::Times>, String> {
-        Ok(vec![self.data.times.clone()])
-    }
-
-    async fn create_times(
+    async fn get(
         &mut self,
-        _title: String,
-    ) -> Result<super::Times, String> {
-        Err("not supported to create times".to_string())
+    ) -> Result<Vec<Arc<Mutex<dyn TimesStore + Send + Sync>>>, String> {
+        todo!();
     }
 
-    async fn delete_times(&mut self, _tid: u64) -> Result<(), String> {
-        Err("not supported to delete times".to_string())
-    }
-
-    async fn update_times(
+    async fn create(
         &mut self,
-        _times: super::Times,
-    ) -> Result<Times, String> {
-        Err("not supported to update times".to_string())
+        title: String,
+    ) -> Result<Arc<Mutex<dyn TimesStore + Send + Sync>>, String> {
+        todo!();
     }
 
-    async fn get_posts(
-        &mut self,
-        tid: u64,
-    ) -> Result<Vec<super::Post>, String> {
-        if self.data.times.id != tid {
-            return Err("unknown tid found".to_string());
-        }
-
-        Ok(self.data.posts.clone())
-    }
-
-    async fn create_post(
-        &mut self,
-        tid: u64,
-        _post: String,
-    ) -> Result<super::Post, String> {
-        if self.data.times.id != tid {
-            return Err("unknown tid found".to_string());
-        }
-
-        Err("not supported to create post".to_string())
-    }
-
-    async fn update_post(
-        &mut self,
-        tid: u64,
-        mut _post: super::Post,
-    ) -> Result<super::Post, String> {
-        if self.data.times.id != tid {
-            return Err("unknown tid found".to_string());
-        }
-
-        Err("not supported to update post".to_string())
-    }
-
-    async fn delete_post(&mut self, tid: u64, _pid: u64) -> Result<(), String> {
-        if self.data.times.id != tid {
-            return Err("unknown tid found".to_string());
-        }
-
-        Err("not supported to delete post".to_string())
-    }
-
-    async fn get_latest_post(
-        &mut self,
-        _tid: u64,
-    ) -> Result<Option<Post>, String> {
-        Ok(None)
-    }
-}
-
-impl JsonStore {
-    pub fn new(times: Times, posts: Vec<Post>) -> Self {
-        Self {
-            data: Data { times, posts },
-        }
-    }
-
-    pub fn build(path: PathBuf) -> Result<Self, String> {
-        let data = Self::load_from_file(path)?;
-
-        Ok(Self { data })
-    }
-
-    pub fn save_to_file(&mut self, filepath: &PathBuf) -> Result<(), String> {
-        let serialized =
-            serde_json::to_string(&self.data).map_err(|e| format!("{e}"))?;
-
-        println!("{serialized}");
-        let file = File::create(filepath).map_err(|e| format!("{e}"))?;
-
-        let mut bw = BufWriter::new(file);
-
-        writeln!(bw, "{serialized}").map_err(|e| format!("{e}"))?;
-
-        Ok(())
-    }
-
-    fn load_from_file(filepath: PathBuf) -> Result<Data, String> {
-        let content =
-            fs::read_to_string(filepath).map_err(|e| format!("{e}"))?;
-
-        println!("{content}");
-
-        serde_json::from_str(&content).map_err(|e| format!("{e}"))
+    async fn delete(&mut self, tid: Tid) -> Result<(), String> {
+        todo!();
     }
 }
